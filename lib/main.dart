37f3a547e7c7ad5fe54cb539e576ced0b2c98572
cristovoxdgm <cristovoxdgm@gmail.com>
2019-07-15 10:11:41 -0300

    buscador de gifs

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+  home:Home(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
---
+++ lib/ui/gif_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:share/share.dart';
+
+class GifPage extends StatelessWidget {
+
+  final Map _gifdata;
+
+  GifPage(this._gifdata);
+
+  @override
+  Widget build(BuildContext context) {
+    print(_gifdata);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_gifdata['title']),
+        backgroundColor:Colors.black ,
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.share),
+            onPressed: (){
+              Share.share(_gifdata['images']['fixed_height']['url']);
+            },
+          ),
+
+        ],
+
+      ),
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Image.network(_gifdata['images']['fixed_height']['url']),
+      ),
+
+    );
+
+  }
+}
